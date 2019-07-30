741f97e06b6532a85533ba9862f0b6900ca015a3
djawlths3 <djawlths4@naver.com>
2019-07-30 21:18:09 +0900

    @Transactional 추가

--- src/main/java/com/cafe24/shopping/service/BascketService.java
+++ src/main/java/com/cafe24/shopping/service/BascketService.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cafe24.shopping.repository.BascketDao;
 import com.cafe24.shopping.vo.BascketVo;
 
-
+@Transactional(rollbackFor=Exception.class)
 @Service
 public class BascketService {
 	@Autowired
--- src/main/java/com/cafe24/shopping/service/ProductService.java
+++ src/main/java/com/cafe24/shopping/service/ProductService.java
@@ -4,12 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cafe24.shopping.repository.ProductDao;
 import com.cafe24.shopping.vo.ProductVo;
 
 
-
+@Transactional(rollbackFor=Exception.class)
 @Service
 public class ProductService {
 	@Autowired
